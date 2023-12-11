9b554dea9ec3f796ea62788da3749209fde67569
777shuang <shuang777@outlook.jp>
2023-12-12 03:07:00 +0900

    効率化

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 
 box_size = 32
 cap = cv2.VideoCapture(0)
-status = 0 #0=ready,1=recording
+recording = False
 frame = cap.read()[1]
 height, width, _ = frame.shape
 timer = 0
@@ -18,27 +18,29 @@ last_frame = frame[top:bottom, left:right]
 
 while(True):
     # Capture frame-by-frame
-    ret, frame = cap.read()
+    _, frame = cap.read()
     pre_frame = frame[top:bottom, left:right]
-    diff = cv2.absdiff(pre_frame, last_frame)
-    #to grayscale
-    diff = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
-    #diffを2値化
-    im_diff_bin = (diff > 32) * 255
-    
-    #平均して白か黒か判断
-    if np.average(im_diff_bin) > 128:
-        status = 0
 
-    last_frame = pre_frame
     # Display the resulting frame
-    if status == 0:
+    if not recording:
         view = cv2.rectangle(frame, (left, top), (right, bottom), (0,255,0))
         view = cv2.putText(view, "Press S to start", (30, height - 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
-    elif status == 1:
+    else:
         view = cv2.rectangle(frame, (left, top), (right, bottom),(0,0,255))
         timer = time.time() - start_time
+        
+        diff = cv2.absdiff(pre_frame, last_frame)
+        #to grayscale
+        diff = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
+        #diffを2値化
+        im_diff_bin = (diff > 32) * 255
+        
+        #平均して白か黒か判断
+        if np.average(im_diff_bin) > 128:
+            recording = False
+    
+    last_frame = pre_frame
 
     view = cv2.putText(view, str(timer), (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
     view = cv2.putText(view, "Press Q to quit", (30, height - 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
@@ -47,7 +49,7 @@ while(True):
     if k == 113:
         break
     elif k == 115:
-        status = 1
+        recording = True
         start_time = time.time()
 
 cv2.destroyAllWindows()
