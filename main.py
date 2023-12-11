5ce50620870cac8b49b453fde5c886d0a528d620
777shuang <shuang777@outlook.jp>
2023-12-11 10:29:42 +0900

    Update

--- main.py
+++ main.py
@@ -2,19 +2,24 @@ import numpy as np
 import cv2
 import time
 
-
-box_size=32
+box_size = 32
 cap = cv2.VideoCapture(0)
 status = 0 #0=ready,1=recording
 frame = cap.read()[1]
-height,width,_ = frame.shape
+height, width, _ = frame.shape
 timer = 0
 start_time = 0
-last_frame = frame[round(height/2 - box_size/2):round(height/2 + box_size/2),round(width/2 - box_size/2):round(width/2 + box_size/2)]
+
+top = round(height / 2 - box_size / 2)
+bottom = round(height / 2 + box_size / 2)
+left = round(width / 2 - box_size / 2)
+right = round(width / 2 + box_size / 2)
+last_frame = frame[top:bottom, left:right]
+
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
-    pre_frame = frame[round(height/2 - box_size/2):round(height/2 + box_size/2),round(width/2 - box_size/2):round(width/2 + box_size/2)]
+    pre_frame = frame[top:bottom, left:right]
     diff = cv2.absdiff(pre_frame, last_frame)
     #to grayscale
     diff = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
@@ -24,17 +29,25 @@ while(True):
     #平均して白か黒か判断
     if np.average(im_diff_bin) > 128:
         status = 0
-        timer = time.time() - start_time
 
     last_frame = pre_frame
     # Display the resulting frame
-    if status == 0:view =  cv2.rectangle(frame,(round(width/2 - box_size/2),round(height/2 - box_size/2)),(round(width/2 + box_size/2),round(height/2 + box_size/2)),(0,255,0))
-    if status == 1:view =  cv2.rectangle(frame,(round(width/2 - box_size/2),round(height/2 - box_size/2)),(round(width/2 + box_size/2),round(height/2 + box_size/2)),(0,0,255))
+    if status == 0:
+        view = cv2.rectangle(frame, (left, top), (right, bottom), (0,255,0))
+        view = cv2.putText(view, "Press S to start", (30, height - 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+    if status == 1:
+        view = cv2.rectangle(frame, (left, top), (right, bottom),(0,0,255))
+        timer = time.time() - start_time
+
     view = cv2.putText(view, str(timer), (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-    cv2.imshow('frame',view)
+    view = cv2.putText(view, "Press Q to quit", (30, height - 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    cv2.imshow('frame', view)
     k = cv2.waitKey(1)
     if k == 113:
         break
     if k ==115:
         status = 1
         start_time = time.time()
+
+cv2.destroyAllWindows()
